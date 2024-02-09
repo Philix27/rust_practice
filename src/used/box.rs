ea63dae187ba4e098b1ec6521e04e83df0cced64
Philix27 <philixbob@gmail.com>
2024-02-09 06:18:24 +0100

    borrowing and ownership

---
+++ src/used/box.rs
@@ -0,0 +1,22 @@
+fn main() {
+    #[derive(Debug)]
+    struct Person {
+        name: String,
+        age: Box<u8>,
+    }
+    let person = Person {
+        name: String::from("Alice"),
+        age: Box::new(20),
+    };
+    // `name` is moved out of person, but `age` is referenced
+    let Person { name, ref age } = person;
+    let mut new_age = age;
+
+    println!("The person's age is {}", age);
+    println!("The person's new age is {}", new_age);
+    println!("The person's name is {}", name);
+    // Error! borrow of partially moved value: `person` partial move occurs
+    //println!("The person struct is {:?}", person);
+    // `person` cannot be used but `person.age` can be used as it is not mov
+    println!("The person's age from person struct is {}", person.age);
+}
